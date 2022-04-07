ac74753e6f379571be4c06214960397c41c187fb
cookiemiro <xoml9598@gmail.com>
2022-04-07 17:59:49 +0900

    foreign key add

--- server/config/config.js
+++ server/config/config.js
@@ -2,9 +2,9 @@ require('dotenv').config();
 
 module.exports = {
   development: {
-    username: process.env.DATABASE_USERNAME,
-    password: process.env.DATABASE_PASSWORD,
-    database: process.env.DATABASE_NAME,
+    username: "root",
+    password: "dbxodml",
+    database: "development",
     host: "127.0.0.1",
     dialect: "mysql"
   },
