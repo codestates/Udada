90f68523badc911a0250588ca7b6b01d341b17d5
parkbomi-77 <vnstn785@naver.com>
2022-04-07 14:30:06 +0900

    footer 컴포넌트 구현

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,12 @@
 import React, { useState } from 'react';
 import Nav from './components/Nav.js';
+import Footer from './components/Footer.js';
 import HomeContainer from './pages/HomeContainer';
 import SitterListContainer from './pages/SitterListContainer';
 import PetListContainer from './pages/PetListContainer';
+import Signup from './pages/Signup';
+import Login from './pages/Login';
+
 //import NotificationCenter from './components/NotificationCenter';
 import './App.css';
 import {
@@ -27,9 +31,12 @@ function App() {
         <Route exact={true} path="/" element={<HomeContainer petUserInfo={petUserInfo} petSitterInfo={petSitterInfo} />} />
         <Route path="/petsitterlist" element={<SitterListContainer petSitterInfo={petSitterInfo} setPetSitterInfo={setPetSitterInfo} />} />
         <Route path="/petlist" element={<PetListContainer petUserInfo={petUserInfo} setPetUserInfo={setPetUserInfo} />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
 
       </Routes>
       {/*<NotificationCenter />*/}
+      <Footer />
     </Router>
   );
 }
