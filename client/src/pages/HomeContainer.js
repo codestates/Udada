dc8f66d4fa58823b60615e90a70438f8e024660a
Hojewl <shyewon1203@gmail.com>
2022-04-08 22:59:44 +0900

    style: 메인 페이지 캐러솔 구성

--- client/src/pages/HomeContainer.js
+++ client/src/pages/HomeContainer.js
@@ -1,15 +1,19 @@
 import React from 'react';
+import { homeImages } from "../assets/images"
+import { Carousel } from 'react-responsive-carousel';
 
 
-function HomeContainer({ petUserInfo, petSitterInfo }) {
-
-
+function HomeContainer() {
 
     return (
         <div id="main-container">
             <div id="main-body">
-                <div id="main-title">소개글</div>
+                <div id="main-carousel">
+                    <Carousel infiniteLoop useKeyboardArrows autoPlay>
+                        {homeImages.map((src, idx) => <img src={src} alt={idx} />)}
 
+                    </Carousel>
+                </div>
             </div>
         </div>
     );
