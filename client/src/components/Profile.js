8d8968afb6d6a3549b6649bec9870521f6f20892
parkbomi-77 <vnstn785@naver.com>
2022-04-14 03:19:06 +0900

    Style:펫시터&일자리 요일,시간 신청하기 UI

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -42,7 +42,71 @@ function Profile({Information, handleLogin, title}) {
                 }
             </div>
             <div id="profile-date">
-                {/* 예약시간 달력 */}API 예약 달력 들어갈 자리 
+                <div className="profile-date-guide">
+                    <span>돌봄을 신청할 요일, <br/>시간을 선택해주세요</span> 
+                </div>
+                <div className="days">
+                    <div className="days-title">✔︎ 가능한 요일을 체크해주세요</div>
+                    <div className="all_days">
+                    <input type="checkbox" id="a1" name="월" />
+                    <label for="a1"><span>Mon</span></label>
+                    <input type="checkbox" id="a2" name="화" />
+                    <label for="a2"><span>Tue</span></label>
+                    <input type="checkbox" id="a3" name="수" />
+                    <label for="a3"><span>Wed</span></label>
+                    <input type="checkbox" id="a4" name="목" />
+                    <label for="a4"><span>Thu</span></label>
+                    <input type="checkbox" id="a5" name="금" />
+                    <label for="a5"><span>Fri</span></label>
+                    <input type="checkbox" id="a6" name="토" />
+                    <label for="a6"><span>Sat</span></label>
+                    <input type="checkbox" id="a7" name="일" />
+                    <label for="a7"><span>Sun</span></label>
+                    </div>
+                </div>
+                <div className="days">
+                      <div className="days-title">✔︎ 가능한 시간을 체크해주세요</div>
+                      <div className="registrationTime-container">
+                        <select name="" id="registrationStartTime">
+                          <option value="">07</option>
+                          <option value="">08</option>
+                          <option value="">09</option>
+                          <option value="">10</option>
+                          <option value="">11</option>
+                          <option value="">12</option>
+                          <option value="">13</option>
+                          <option value="">14</option>
+                          <option value="">15</option>
+                          <option value="">16</option>
+                          <option value="">17</option>
+                          <option value="">18</option>
+                          <option value="">19</option>
+                          <option value="">20</option>
+                          <option value="">21</option>
+                          <option value="">22</option>
+                        </select><span>시 부터 </span>
+
+                        <select name="" id="registrationLastTime">
+                          <option value="">07</option>
+                          <option value="">08</option>
+                          <option value="">09</option>
+                          <option value="">10</option>
+                          <option value="">11</option>
+                          <option value="">12</option>
+                          <option value="">13</option>
+                          <option value="">14</option>
+                          <option value="">15</option>
+                          <option value="">16</option>
+                          <option value="">17</option>
+                          <option value="">18</option>
+                          <option value="">19</option>
+                          <option value="">20</option>
+                          <option value="">21</option>
+                          <option value="">22</option>
+                        </select><span>시까지 가능해요</span>
+                      </div>
+                </div>
+
             </div>
             <button id="profile-btn" onClick={() => show()}>신청하기</button>
 
